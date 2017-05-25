6afe0b2e69214b2aa341d6775050abf7395b4629
Vivek Singh <petmongrels@gmail.com>
2017-05-25 17:33:30 +0530

    renamed dump files

--- db/take-db-backup.sh
+++ db/take-db-backup.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DAYNAME=$(date +%a)
-pg_dump facility_assessment > ${SCRIPT_DIR}/backup/facility_asessment_dump_${DAYNAME}.sql
-pg_dump facility_assessment_cg > ${SCRIPT_DIR}/backup/facility_asessment_cg_dump_${DAYNAME}.sql
-pg_dump facility_assessment > ${SCRIPT_DIR}/backup/facility_asessment_metabase_dump_${DAYNAME}.sql
+pg_dump facility_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_dump_${DAYNAME}.sql
+pg_dump facility_assessment_cg > ${SCRIPT_DIR}/backup/facilities_asessment_cg_dump_${DAYNAME}.sql
+pg_dump facility_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_metabase_dump_${DAYNAME}.sql
