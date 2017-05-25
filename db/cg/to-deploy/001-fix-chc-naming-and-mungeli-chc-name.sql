ed42f63d01b23a81802abbec041846f1c42baf63
Vivek Singh <petmongrels@gmail.com>
2017-05-25 18:19:02 +0530

    renaming chc facility types and facility for CH

---
+++ db/cg/to-deploy/001-fix-chc-naming-and-mungeli-chc-name.sql
@@ -0,0 +1,2 @@
+update facility_type set name = 'Community Health Center', last_modified_date = current_timestamp WHERE name = 'Community Hospital';
+update facility SET name = 'CHC Lormi', last_modified_date = current_timestamp WHERE name = 'Mungeli Community Hospital';
