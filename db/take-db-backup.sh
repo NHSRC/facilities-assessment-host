1a8ea9bd7b16eaef2d92c4ca4d9a05293b83b9f1
Vivek Singh <petmongrels@gmail.com>
2017-05-27 15:36:48 +0530

    echoing script dir

--- db/take-db-backup.sh
+++ db/take-db-backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo 'SCRIPT_DIR'
+echo ${SCRIPT_DIR}
 
 DAYNAME=$(date +%a)
 pg_dump facilities_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_dump_${DAYNAME}.sql
