ff8847db12c1197cebc33b38d90c7a8cff363b79
Vivek Singh <petmongrels@gmail.com>
2017-05-29 20:34:26 +0530

    logging the location of back

--- db/take-db-backup.sh
+++ db/take-db-backup.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$(dirname "$0")
 
 DAYNAME=$(date +%a)
-echo "[backup] Backing up postgres databases $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
+echo "[backup] Backing up postgres databases as ${SCRIPT_DIR}/backup with ${DAYNAME}" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
 pg_dump facilities_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_dump_${DAYNAME}.sql
 echo "[backup] Backed up facilities_assessment $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
 pg_dump facilities_assessment_cg > ${SCRIPT_DIR}/backup/facilities_asessment_cg_dump_${DAYNAME}.sql
