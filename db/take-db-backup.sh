06dee83adf805168a62d178c84a01989bf752903
Vivek Singh <petmongrels@gmail.com>
2017-05-29 20:32:26 +0530

    additional log statements earlier the log was getting the dump

--- db/take-db-backup.sh
+++ db/take-db-backup.sh
@@ -3,5 +3,7 @@ SCRIPT_DIR=$(dirname "$0")
 
 DAYNAME=$(date +%a)
 echo "[backup] Backing up postgres databases $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
-pg_dump facilities_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_dump_${DAYNAME}.sql >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
-pg_dump facilities_assessment_cg > ${SCRIPT_DIR}/backup/facilities_asessment_cg_dump_${DAYNAME}.sql >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
+pg_dump facilities_assessment > ${SCRIPT_DIR}/backup/facilities_asessment_dump_${DAYNAME}.sql
+echo "[backup] Backed up facilities_assessment $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
+pg_dump facilities_assessment_cg > ${SCRIPT_DIR}/backup/facilities_asessment_cg_dump_${DAYNAME}.sql
+echo "[backup] Backed up facilities_assessment_cg $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1 &
