cd9526ca86a31c39f50fa6d6783dc955f449ab99
mihirk <mihir.khatwani@gmail.com>
2017-06-14 12:02:51 +0530

    Fixing the cron job scheduling and making the scripts executable

--- schedule-backup.sh
+++ schedule-backup.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #write out current crontab
 crontab -l > dbbackupcron || true
 #2am daily all dbs
-echo "0 0 2 * * ? /bin/sh ${SCRIPT_DIR}/db/take-db-backup.sh" >> dbbackupcron
+echo "0 0 2 * * ? ${SCRIPT_DIR}/db/take-db-backup.sh" >> dbbackupcron
 #3am daily metabase db
-echo "0 0 3 * * ? /bin/sh ${SCRIPT_DIR}/metabase/take-db-backup.sh" >> dbbackupcron
+echo "0 0 3 * * ? ${SCRIPT_DIR}/metabase/take-db-backup.sh" >> dbbackupcron
 #3am every week entire cluster
 #echo "0 0 3 1/7 * ? echo hello" >> dbbackupcron
 #install new cron file
