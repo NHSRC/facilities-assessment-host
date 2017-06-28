f8d0c224b74b1127c2e7bbfd0e04bd97fe73d98d
Vivek Singh <petmongrels@gmail.com>
2017-06-28 16:26:42 +0530

    fixed day name error in the backup file

--- metabase/take-db-backup.sh
+++ metabase/take-db-backup.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$(dirname "$0")
 DAYNAME=$(date +%a)
 
 echo "[backup] Backing up metabase $(date)" >> ${SCRIPT_DIR}/log/backup.log 2>&1
-cp ${SCRIPT_DIR}/metabase.db.mv.db ${SCRIPT_DIR}/backup/metabase.db.mv.db_DAYNAME >> ${SCRIPT_DIR}/log/backup.log 2>&1
+cp ${SCRIPT_DIR}/metabase.db.mv.db ${SCRIPT_DIR}/backup/metabase.db.mv.db_${DAYNAME} >> ${SCRIPT_DIR}/log/backup.log 2>&1
