cef9364ec3113dd709eb4c812c42deb01af105f5
Vivek Singh <petmongrels@gmail.com>
2017-07-17 14:08:03 +0530

    init.d at JSS

---
+++ deployments/jss/init.d
@@ -0,0 +1,32 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          fa
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: fa
+# Description:       fa
+### END INIT INFO
+#Facilities Assessment init script
+
+HOME=/home/nhsrc/facilities-assessment-host
+
+case $1 in
+start)
+        cd ${HOME} && sh metabase/run.sh
+        cd ${HOME} && sh app-servers/cg/run.sh
+        cd ${HOME} && sh app-servers/mp/run.sh
+        ;;
+stop)
+        pkill -f 'java -jar metabase.jar'
+        pkill -f 'database=new_facilitiess_assessment_mp'
+        pkill -f 'database=facilities_assessment_cg'
+        ;;
+restart)
+        stop
+        sleep 5
+        start
+        ;;
+esac
+exit 0
