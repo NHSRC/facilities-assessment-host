c7eb7c71554323cd151ce59d3c64378224c4aa9d
Vivek Singh <petmongrels@gmail.com>
2017-08-08 23:10:12 +0530

    no more mp server needed

--- deployments/jss/init.d
+++ deployments/jss/init.d
@@ -8,12 +8,10 @@ start)
         sleep 10
         cd ${HOME} && make start_metabase
         cd ${HOME} && make jss_cg_start_server
-        cd ${HOME} && make jss_mp_start_server
         ;;
 stop)
         cd ${HOME} && make stop_metabase
         cd ${HOME} && make jss_cg_stop_server
-        cd ${HOME} && make jss_mp_stop_server
         ;;
 restart)
         stop
