93f9221c41040ed705a8a23b7ac973a502e41188
Vivek Singh <petmongrels@gmail.com>
2018-06-04 10:27:59 +0530

    jss specific files

--- deployments/jss/copy-backups.sh
+++ deployments/jss/copy-backups.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 DAYNAME=$(date +%a)
 scp /home/nhsrc/facilities-assessment-host/db/backup/facilities_assessment_cg_${DAYNAME}.sql bahmni_support@192.168.0.157:/home/bahmni_support/igunatmacbackup/
-scp /home/nhsrc/facilities-assessment-host/metabase/metabase.db.mv.db_${DAYNAME} bahmni_support@192.168.0.157:/home/bahmni_support/igunatmacbackup/
+scp /home/nhsrc/facilities-assessment-host/metabase/metabase.db.mv.db bahmni_support@192.168.0.157:/home/bahmni_support/igunatmacbackup/metabase.db.mv.db_${DAYNAME}
+
+SCRIPT_DIR=$(dirname "$0")
+LOG_FILE=${SCRIPT_DIR}/log/backup.log
+
+echo "[backup][$(date)] Backedup postgres databases at 157" >> ${LOG_FILE} 2>&1 &
