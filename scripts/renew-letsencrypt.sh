2c60b64d239fd4b49f5aed5607e74bb408cecbf0
Charles <charlesj@gmail.com>
2019-02-04 01:28:39 +0530

    Refactor letsencrypt renewal scripts

--- scripts/renew-letsencrypt.sh
+++ scripts/renew-letsencrypt.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ $(id -u) -ne 0 ]] ; then echo "root/sudo privileges required" ; exit 1 ; fi
+if [[ -z "${FQDN}" ]]; then echo "env var \$FQDN is unset" ; exit 1; fi
+if [[ -z "${PASSWORD}" ]]; then echo "env var \$PASSWORD (keystore) is unset" ; exit 1; fi
 
 USER=app
 PROJECT_DIR=$(dirname $(readlink -f $(dirname "$0")))
@@ -10,10 +12,16 @@ P12_NAME='keystore'
 JKS_NAME=${P12_NAME}
 CERTBOT_OUTPUT=/tmp/crt.txt
 
-renew_ssl() {
-    systemctl stop ${ENV_PREFIX}-fab
+# expecting ENV_PREFIX to come from the environment. If unset, prod will be assumed
 
-    certbot renew > ${CERTBOT_OUTPUT}
+renew_ssl() {
+    systemctl stop ${ENV_PREFIX}fab
+
+    certbot renew &> ${CERTBOT_OUTPUT}
+
+    grep -q "Cert not yet due for renewal" ${CERTBOT_OUTPUT}
+
+    if [ $? -eq 0 ]; then exit 1; fi
 
     openssl pkcs12 -export -out ${APP_DIR}/${P12_NAME}.p12 \
     -passin pass:${PASSWORD} -passout pass:${PASSWORD} \
@@ -30,8 +38,8 @@ renew_ssl() {
     chown ${USER}:${USER} ${APP_DIR}/${P12_NAME}.p12
     chown ${USER}:${USER} ${METABASE_DIR}/${JKS_NAME}.jks
 
-    systemctl start ${ENV_PREFIX}-fab
-    systemctl restart ${ENV_PREFIX}-metabase
+    systemctl start ${ENV_PREFIX}fab
+    systemctl restart ${ENV_PREFIX}metabase
 }
 
 send_mail_notification() {
@@ -42,8 +50,6 @@ send_mail_notification() {
     --region us-east-1
 
 }
-read -p "Enter fully qualified domain name: " FQDN
-read -p "keystore password: " PASSWORD
-read -p "environment name, ignore if production: " ENV_PREFIX
+
 renew_ssl
 send_mail_notification
